d4be3d6bf314e454182a2ab13601c7f6444c1d3a
nitinsoni967 <34713241+nitinsoni967@users.noreply.github.com>
2019-11-27 11:30:31 +0530

    Updates

---
+++ precache-manifest.9c5952fc9ac38a08488f385a1a64fbb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a43142938ff5253100f9298a5ce2ff",
+    "url": "/react-todos-new/index.html"
+  },
+  {
+    "revision": "100aeb98d9f5a5bea41d",
+    "url": "/react-todos-new/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fe1038ec430155fed5b0",
+    "url": "/react-todos-new/static/js/2.7333de81.chunk.js"
+  },
+  {
+    "revision": "100aeb98d9f5a5bea41d",
+    "url": "/react-todos-new/static/js/main.b00e756c.chunk.js"
+  },
+  {
+    "revision": "79710a57bbfdd2a89a83",
+    "url": "/react-todos-new/static/js/runtime-main.b91b2202.js"
+  },
+  {
+    "revision": "3e8039242e517ee7edc05a4e226e0b80",
+    "url": "/react-todos-new/static/media/loader1.3e803924.gif"
+  }
+]);
